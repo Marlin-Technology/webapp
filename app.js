2973a4402dc41a43b54fce3edeef4519355b13d8
Raphael910401 <rafa2006.faint@gmail.com>
2017-05-07 16:12:05 -0700

    v0.1.5

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ if ('development' == env) {
    app.set('port', process.env.PORT || 3000);
    app.set('views', __dirname + '/views');
    app.set('view engine', 'ejs');
-   app.use(favicon(path.join(__dirname, 'public', 'images/favicon.ico')));
+   app.use(favicon(path.join(__dirname, 'public', '/images/favicon.ico')));
    app.use(morgan('dev'));
    app.use(bodyParser());
    app.use(methodOverride());
@@ -32,6 +32,8 @@ if ('development' == env) {
    app.use(session());
    app.use(serveStatic(path.join(__dirname, 'public')));
 }
+
+app.get('/',routes.index);
 
 http.createServer(app).listen(app.get('port'),function()
 {
